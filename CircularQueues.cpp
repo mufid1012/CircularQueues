3c29bde21d82f10d404afe6752f71797bcbea1b1
mufid1012 <mufidghibran1012@gmail.com>
2024-05-30 15:33:54 +0700

    membuat if untuk mengecek apakah antrian penuh

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -19,6 +19,12 @@ private:
         cin >> num;
         cout  << endl;
 
+        //cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
 
+        
 };
 
